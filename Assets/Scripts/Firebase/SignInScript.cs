31668ef4c3e12928b461dd04cb98c358f47ad249
ziudull <31065936+ziudull@users.noreply.github.com>
2017-08-20 23:57:23 -0700

    first commit
    setting user display name

---
+++ Assets/Scripts/Firebase/SignInScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignInScript : MonoBehaviour {
+
+public Firebase.Auth.FirebaseAuth auth;
+public Text setDisplayName;
+
+	// Use this for initialization
+	void Start () {
+		auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+	}
+
+	public void SetDisplayName(){
+		Debug.Log(setDisplayName);
+
+		Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+		if (user != null) {
+			Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile {
+				DisplayName = setDisplayName.ToString()
+				//,
+				//PhotoUrl = new System.Uri("https://example.com/jane-q-user/profile.jpg"),
+			};
+		user.UpdateUserProfileAsync(profile).ContinueWith(task => {
+			if (task.IsCanceled) {
+			Debug.LogError("UpdateUserProfileAsync was canceled.");
+			return;
+			}
+			if (task.IsFaulted) {
+			Debug.LogError("UpdateUserProfileAsync encountered an error: " + task.Exception);
+			return;
+			}
+
+			Debug.Log("User profile updated successfully.");
+		});
+}
+	}
+
+	// Use this to for users to sign in anonymously
+	public void SignInAnon(){
+		auth.SignInAnonymouslyAsync().ContinueWith(task => {
+		if (task.IsCanceled) {
+			Debug.LogError("SignInAnonymouslyAsync was canceled.");
+			return;
+		}
+		if (task.IsFaulted) {
+			Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+			return;
+		}
+
+		Firebase.Auth.FirebaseUser newUser = task.Result;
+		SetDisplayName();
+		Debug.LogFormat("User signed in successfully: {0} ({1})", newUser.DisplayName, newUser.UserId);
+		});
+	}
+
+	public void SignInFB(){
+
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+
+public GameObject GOPage;
+float playerShakeY;
+public Text shakeValueText;
+public Text timerText;
+public Text gotimerText;
+
+float previousShake;
+float currentShake;
+float time;
+
+	// Use this for initialization
+	void Start () {
+		previousShake = Input.acceleration.y;
+		//shakeValueText.text = playerShakeY.ToString();
+	}
+
+	void Update(){
+		if (transform.position.y < 3.75){
+			Timer();
+		}
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (transform.position.y < 3.75){
+			Shake();
+			shakeValueText.text = currentShake.ToString("F3");
+		}
+		else{
+			gotimerText.text = timerText.text.ToString();
+			GOPage.SetActive(true);
+		}
+	}
+
+	void Shake(){
+		currentShake = Input.acceleration.y;
+		if (currentShake < 0){
+			//do nothing if shake strength is less than 0
+		}
+		else if(currentShake != previousShake) {
+			//move at shake strength
+			transform.position += new Vector3(0, Mathf.Abs(currentShake), 0) * 2F * Time.deltaTime;
+		}
+		previousShake = currentShake;
+	}
+
+	void Timer(){
+		timerText.text = Time.time.ToString("F2");
+	}
+}
